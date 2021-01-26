350f5d0ec8db4c9590384b8abd8120bdec41de18
iankure <61777971+iankure@users.noreply.github.com>
2021-01-26 08:18:13 -0800

    Add files via upload

---
+++ modules/ec2/instances.tf
@@ -0,0 +1,12 @@
+# from www.terraform.io/docs/providers/aws/r/instance.html
+
+resource "aws_instance" "web" {
+  count         = "${var.ec2_count}"
+  ami           = "${var.ami_id}"
+  instance_type = "${var.instance_type}"
+  subnet_id     = "${var.subnet_id}"
+
+  tags {
+    Name = "HelloWorld"
+  }
+}
